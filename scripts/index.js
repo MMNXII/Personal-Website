88788a96acbd5404e4075e07d19e01b1c7712507
Matthew Neal <matthewneal1202@gmail.com>
2020-04-17 11:27:27 -0700

    shortened intro animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,12 @@
-
 (introAnime = () => {
-
-    var animation = anime({
-    targets: "#header, #profile, #intro-container, #work-container, #icon-container",
+  var animation = anime({
+    targets:
+      '#header, #profile, #intro-container, #work-container, #icon-container',
     duration: 500,
-    delay: anime.stagger(500, { start: 300, from: "first" }),
-    easing: "easeInOutSine",
+    delay: anime.stagger(300, { start: 300, from: 'first' }),
+    easing: 'easeInOutSine',
     opacity: [0, 1],
-    translateY: ["-1rem", 0],
-    direction: "normal",
-});
-
-
+    translateY: ['-1rem', 0],
+    direction: 'normal',
+  });
 })();
