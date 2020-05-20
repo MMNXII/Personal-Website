17c492db3a1896237ae7db23f9d42ebf538f81fd
Matthew Neal <matthewneal1202@gmail.com>
2020-05-20 15:53:09 -0700

    revised icons

--- scripts/index.js
+++ scripts/index.js
@@ -517,7 +517,7 @@ const webPageContent = (function () {
     createHeader('About');
     createContentContainer('about-content');
 
-    const about1 = document.createElement('h5');
+    const about1 = document.createElement('p');
     about1.textContent = 'Designer at heart, Developer on the rise.';
     about1.className = 'header content-intro';
     contentContainer.appendChild(about1);
