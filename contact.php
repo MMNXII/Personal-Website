538a9f3c348de92b5cd7dad6132fb615f2dfa4f4
Matthew Neal <matthewneal1202@gmail.com>
2020-06-01 10:56:57 -0700

    email test

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    
+    $name = $_POST['name_input'];
+    $mail = $_POST['email_input'];
+    $message = $_POST['message_input'];
+
+    $mailTo = 'matthewneal6@yahoo.com.com';
+    $headers = 'From: ' .$mail;
+
+    $txt = 'Email from ' .$name.'.\n\n'.$message;
+
+    mail($mailTo, $txt, $headers);
+    header('Location: index.php?mailsend');
+
+}
+
+?>
